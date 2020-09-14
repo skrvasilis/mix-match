318ff43ad23ffe71cd3c15e03733dbe7b5db10c7
mrcll <passes@protonmail.com>
2020-09-14 14:29:16 +0200

    Home and Welcome added

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import Logo from "../assets/Spotify_Logo_RGB_Green.png"
 import MyContext from "../MyContext";
 
 export default function Home() {
@@ -7,17 +8,17 @@ export default function Home() {
   );
 
   return (
-    <div>
-      <div>
-        <a href="http://localhost:8888"> Login to Spotify </a>
+    <div className="main">
+      <div className="content">
+      <h3>Welcome to</h3>
+      <h1>Mix & Match</h1>
+      <a href="http://localhost:8888">
+        <h3>Please login using your</h3>
+        <img className="logo" src={Logo} alt="Spotify Logo" />
+  
+        <p>to find your real music Buddy!</p>
+      </a>
       </div>
-      {loggedIn && (
-        <div>
-          <button onClick={() => fetchDataFromSpotify()}>
-            Fetch Data From Spotify
-          </button>
-        </div>
-      )}
     </div>
   );
 }
