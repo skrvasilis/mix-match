eac69f4f1529788589696e7d642f56d1d5a5e505
skrvasilis <skrvasilis@gmail.com>
2020-09-14 12:48:01 +0200

    hooksfinal

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,83 +1,14 @@
 import React from "react";
 import "./App.css";
-import Container from './Container'
-import Home from './Home'
-
-
+import Container from "./Container";
+import Home from "./Home";
 
 function App() {
- 
-
-  /*   const params = this.getHashParams();
-    this.token = params.access_token;
-    console.log(params, this.token);
-    if (this.token) {
-      spotifyApi.setAccessToken(this.token);
-    }
-    this.state = {
-      loggedIn: this.token ? true : false,
-      nowPlaying: { name: "Not Checked", albumArt: "" },
-    };
-  }
-  getHashParams() {
-    var hashParams = {};
-    var e,
-      r = /([^&;=]+)=?([^&;]*)/g,
-      q = window.location.hash.substring(1);
-    e = r.exec(q);
-    while (e) {
-      hashParams[e[1]] = decodeURIComponent(e[2]);
-      e = r.exec(q);
-    }
-    return hashParams;
-  }
- */
- /*  getNowPlaying() {
-    spotifyApi.getMyCurrentPlaybackState().then((response) => {
-      this.setState({
-        nowPlaying: {
-          name: response.item.name,
-          albumArt: response.item.album.images[0].url,
-        },
-      });
-    });
-  } */
-  /* fetchDataFromSpotify = () => {
-    fetch("https://api.spotify.com/v1/me/top/artists?time_range=medium_term&limit=20&offset=5", {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + this.token,
-      },
-    }).then((response) => {
-      console.log(
-        response.json().then((data) => {
-          this.setState({ data: data });
-          console.log(data);
-        })
-      );
-    });
-  };
-
-
-
-
-
-
-
-  render() {
-    if (this.state.data) {
-      console.log(this.state);
-    } */
-    return (
-     
-        <Container>
-        <Home/>
-        </Container>
-    
-    );
-  }
-
+  return (
+    <Container>
+      <Home />
+    </Container>
+  );
+}
 
 export default App;
--- client/src/Home.js
+++ client/src/Home.js
@@ -1,23 +1,23 @@
-import React ,{useContext} from 'react'
-import MyContext from './MyContext'
+import React, { useContext } from "react";
+import MyContext from "./MyContext";
 
 export default function Home() {
-  const { data, setData, fetchDataFromSpotify,loggedIn } = useContext(MyContext);
+  const { data, setData, fetchDataFromSpotify, loggedIn } = useContext(
+    MyContext
+  );
 
-    return (
-        <div>
-        <div>
+  return (
+    <div>
+      <div>
         <a href="http://localhost:8888"> Login to Spotify </a>
+      </div>
+      {loggedIn && (
+        <div>
+          <button onClick={() => fetchDataFromSpotify()}>
+            Fetch Data From Spotify
+          </button>
         </div>
-        {loggedIn && (
-          <div>
-            <button onClick={() => fetchDataFromSpotify()}>
-              Fetch Data From Spotify
-            </button>
-        </div>
-    )
-}
-</div>
-
-    )
+      )}
+    </div>
+  );
 }
