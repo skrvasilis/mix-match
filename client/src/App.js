192a4558ca285d9748e2ccd1a7ed712f3f795bab
skrvasilis <skrvasilis@gmail.com>
2020-09-14 10:43:55 +0200

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,91 +1,94 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-import SpotifyWebApi from 'spotify-web-api-js';
+import SpotifyWebApi from "spotify-web-api-js";
 const spotifyApi = new SpotifyWebApi();
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state={
-      data:null
-    }
+    this.state = {
+      data: null,
+    };
+
     const params = this.getHashParams();
     this.token = params.access_token;
-    console.log(params,this.token)
+    console.log(params, this.token);
     if (this.token) {
       spotifyApi.setAccessToken(this.token);
     }
     this.state = {
       loggedIn: this.token ? true : false,
-      nowPlaying: { name: 'Not Checked', albumArt: '' }
-    }
+      nowPlaying: { name: "Not Checked", albumArt: "" },
+    };
   }
   getHashParams() {
     var hashParams = {};
-    var e, r = /([^&;=]+)=?([^&;]*)/g,
-        q = window.location.hash.substring(1);
-    e = r.exec(q)
+    var e,
+      r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    e = r.exec(q);
     while (e) {
-       hashParams[e[1]] = decodeURIComponent(e[2]);
-       e = r.exec(q);
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+      e = r.exec(q);
     }
     return hashParams;
   }
 
-  getNowPlaying(){
-    spotifyApi.getMyCurrentPlaybackState()
-      .then((response) => {
-        this.setState({
-          nowPlaying: { 
-              name: response.item.name, 
-              albumArt: response.item.album.images[0].url
-            }
-        });
-      })
+  getNowPlaying() {
+    spotifyApi.getMyCurrentPlaybackState().then((response) => {
+      this.setState({
+        nowPlaying: {
+          name: response.item.name,
+          albumArt: response.item.album.images[0].url,
+        },
+      });
+    });
   }
-fetchDataFromSpotify=()=>{
-  fetch('https://api.spotify.com/v1/artists/21E3waRsmPlU7jZsS13rcj', {
-            method: 'GET', headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + this.token
-            }
+  fetchDataFromSpotify = () => {
+    fetch("https://api.spotify.com/v1/me/top/artists?time_range=medium_term&limit=20&offset=5", {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + this.token,
+      },
+    }).then((response) => {
+      console.log(
+        response.json().then((data) => {
+          this.setState({ data: data });
+          console.log(data);
         })
-            .then((response) => {
-                console.log(response.json().then(
-                    (data) => {this.setState({data:data}); console.log(data) }
-                ));
-            });
-}
+      );
+    });
+  };
   render() {
+    if (this.state.data) {
+      console.log(this.state.data);
+    }
     return (
       <div className="App">
-        <a href='http://localhost:8888' > Login to Spotify </a>
+        <a href="http://localhost:8888"> Login to Spotify </a>
+        <div>Now Playing: {this.state.nowPlaying.name}</div>
         <div>
-          Now Playing: { this.state.nowPlaying.name }
+          <img src={this.state.nowPlaying.albumArt} style={{ height: 150 }} />
         </div>
-        <div>
-          <img src={this.state.nowPlaying.albumArt} style={{ height: 150 }}/>
-        </div>
-        { this.state.loggedIn &&
-       
-          <div><button onClick={() => this.fetchDataFromSpotify()}>
-            Fetch Data From Spotify
-          </button>
-          {this.state.data?<div>
-            <h2>Artist Name: {this.state.data.name} </h2>
-          <h3>Popularity: {this.state.data.popularity}</h3>
-          {this.state.data.images.map(img=>{
-            return (
-              <img src={img.url} width={img.width}/>
-            )
-          })}
-          </div>:null}
+        {this.state.loggedIn && (
+          <div>
+            <button onClick={() => this.fetchDataFromSpotify()}>
+              Fetch Data From Spotify
+            </button>
+            {this.state.data ? (
+              <div>
+                {/* <h2>Artist Name: {this.state.data.name} </h2>
+                <h3>Popularity: {this.state.data.popularity}</h3>
+                {this.state.data.images.map((img) => {
+                  return <img src={img.url} width={img.width} />;
+                })} */}
+              </div>
+            ) : null}
           </div>
-        
-          
-        }
+        )}
       </div>
     );
   }
