396128921719368689fdb5721e25454acf116e2d
skrvasilis <skrvasilis@gmail.com>
2020-09-17 16:20:41 +0200

    matchingdata

--- client/src/components/MyMatchesAll.jsx
+++ client/src/components/MyMatchesAll.jsx
@@ -1,51 +1,97 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import MyContext from "../MyContext";
 
 export default function MyMatch() {
-  const { dataBase,userData,topGenres, allArtists} = useContext(MyContext);
+  const { dataBase, userData, topGenres, allArtists } = useContext(MyContext);
+  const[genresSorted, setGenresSorted] = useState([])
+
   useEffect(() => {
-    console.log(topGenres)
-    console.log(allArtists)
     if (dataBase[0]) {
-  let current= dataBase.filter((item)=>item.userName!==userData.display_name)
-console.log(current)
+      // here we exclude the current user from the database
+      let currentDataBase = dataBase.filter(
+        (item) => item.userName !== userData.display_name
+      );
+      console.log(currentDataBase);
+
+      // make objects with the users from the database
+
+      // i get the same artists but i cannot combine the users
+      let sameArtists = [];
+
+      currentDataBase.map((item) => {
+        sameArtists.push({
+          userName: item.userName,
+          sameArtists: item.userTracks.filter((value) =>
+            allArtists.includes(value)
+          ),
+        });
+      });
+
+      // sort from the biggest
+      let sameArtistSorted = sameArtists.sort((a,b)=> b.sameArtists.length-a.sameArtists.length)
+      console.log(sameArtistSorted)
+      
+
+
+
+      // here i get an array with all the genres the current user has
+      let currentGenres = [];
+      topGenres.map((item) => {
+        currentGenres.push(...item);
+      });
+      console.log(currentGenres);
+
+      // here i get 2 arrays with the genres that the others users have
+      let otherGenres = [];
+      currentDataBase.map((item) => {
+        otherGenres.push([...item.userGenres]);
+      });
+
+      // here i get the same artists but not the userName as well
+      let sameGenres = [];
+      for (const item of otherGenres) {
+        item.join().split(",");
+        sameGenres.push(
+          item
+            .join()
+            .split(",")
+            .filter((value) => currentGenres.includes(value))
+        );
+      }
+
+      // here we delete duplicates
+      let uniqueGenres = [];
+      for (const item of sameGenres) {
+        uniqueGenres.push([...new Set(item)]);
+      }
+      console.log(uniqueGenres);
+
+      for (const item of uniqueGenres) {
+        console.log(currentDataBase[uniqueGenres.indexOf(item)].userName);
+      }
+      let finalSameGenres = [];
+      for (const item of uniqueGenres) {
+        finalSameGenres.push({
+          userName: currentDataBase[uniqueGenres.indexOf(item)].userName,
+          sameGenres: item,
+        });
+      }
+      console.log(finalSameGenres);
+
     
 
-      let sameArtists =[]
-      for (const item of current) {
-         sameArtists.push((item.userTracks.filter((value)=>allArtists.includes(value))))
-      }
-      console.log(sameArtists)
-
-    
-      let sameGenres =[]
-      let currentGenres = []
-      topGenres.map((item)=>{
-        currentGenres.push(...item)
-      })
-      console.log(currentGenres)
-    let otherGenres = []
-      for (const item of current) {
-      item.userGenres.map((value)=>otherGenres.push(...value))
-      }
-console.log(otherGenres)
-for (const item of current) {
-   sameGenres.push((currentGenres.filter((value)=>otherGenres.includes(value))))
-}
-
- 
-console.log(sameGenres)
-
-
-let unique = []
-for (const item of sameGenres)
- unique.push([...new Set(item)])
-console.log(unique)
-
+// we sort the users from the biggest
+     let test = finalSameGenres.sort((a,b)=> b.sameGenres.length-a.sameGenres.length)
+     setGenresSorted(test)
+console.log(genresSorted)
 
     }
   }, [dataBase]);
+  if (genresSorted) {
+    console.log(genresSorted)
+
+  }
 
   return (
     <div className="main">
@@ -58,14 +104,18 @@ console.log(unique)
         />
         <ul className="results">
           <Link to="/mymatch">
-            <li>
-              <h4>Number 1</h4>
-              <img
-                className="list-avatar"
-                src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png"
-                alt="avatar"
-              />
-            </li>
+              {genresSorted && genresSorted.map((item)=>{
+                return (
+                  <li>
+                  <h4>{item.userName}</h4>
+                  <img
+                    className="list-avatar"
+                    src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png"
+                    alt="avatar"
+                  />
+                </li>
+                )
+              })}
           </Link>
         </ul>
       </div>
